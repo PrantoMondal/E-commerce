f464d27deac9662a4d5126ca2598d3330f87d60a
PrantoMondal <prantomondal517@gmail.com>
2022-05-17 11:56:02 +0600

    Authentication Done

--- app/src/main/java/com/example/ecomadmin/DashboardFragment.kt
+++ app/src/main/java/com/example/ecomadmin/DashboardFragment.kt
@@ -5,20 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.ecomadmin.adapters.DashboardItemAdapters
-import com.example.ecomadmin.databinding.DashboardRowBinding
 import com.example.ecomadmin.databinding.FragmentDashboardBinding
+import com.example.ecomadmin.viewmodels.LoginViewModel
 
 
 class DashboardFragment : Fragment() {
 
     private lateinit var binding: FragmentDashboardBinding
+    private val userViewModel:LoginViewModel by activityViewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentDashboardBinding.inflate(inflater,container,false)
+        userViewModel.authLD.observe(viewLifecycleOwner){
+            if (it == LoginViewModel.Authentication.UNAUTHENTICATED){
+                findNavController().navigate(R.id.action_dashboardFragment_to_loginFragment)
+            }
+        }
+
         val adapter = DashboardItemAdapters()
         val glm = GridLayoutManager(requireActivity(),2)
         binding.dashboardRV.layoutManager = glm
